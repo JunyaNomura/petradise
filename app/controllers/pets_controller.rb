622bfa203174b37955c3eec808e9f6a73bd9931f
Antonio Sanchez <antonio.sanchez.trespalacios@gmail.com>
2020-03-03 19:29:33 +0900

    testing almost finished

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,50 @@
 class PetsController < ApplicationController
+  before_action :set_pet, only: [:show, :edit, :update, :delete]
+  def index
+    @pets = Pet.all
+  end
+
+  def show
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to pets_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @pet.update(pet_params)
+      redirect_to pets_path
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    @pet.delete
+    redirect_to pets_path
+  end
+
+
+  private
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+
+  def pet_params
+    params.require(:pet).permit(:name, :gender, :age, :breed, :description)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :pets
   has_many :ratings
-  validates :first_name, :last_name, :location, presence: true
+  # validates :first_name, :last_name, :location, presence: true
 end
