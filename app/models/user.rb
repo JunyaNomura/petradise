76e8a7db9eb25f61b89b71968833238c88b9cd37
Antonio Sanchez <antonio.sanchez.trespalacios@gmail.com>
2020-03-04 18:16:43 +0900

    landing finished but we need to change the picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one :pet
   has_many :ratings
+  has_one_attached :photo
   has_friendship
   # validates :first_name, :last_name, :location, presence: true
   # exclude friend pets - current_user.friends
