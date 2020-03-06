4672c1ec65c565a9affeb7cd35601eb92321426a
Antonio Sanchez <antonio.sanchez.trespalacios@gmail.com>
2020-03-06 14:16:03 +0900

    sweet alert and minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,14 @@ class UsersController < ApplicationController
 
   def friend_accept
     target_user = User.find(params[:id])
-    target_user.accept_request(current_user)
+    current_user.accept_request(target_user)
     redirect_to pets_path
   end
 
   def friend_reject
     target_user = User.find(params[:id])
     # target_user.decline_request(current_user)
-    target_user.block_friend(current_user)
+    current_user.block_friend(target_user)
     redirect_to pets_path
   end
 
