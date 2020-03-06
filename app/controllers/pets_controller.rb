acf5408a09df4940932c52d7a2ca9ea54e8c25e0
JunyaNomura <jm18l028@g.hit-u.ac.jp>
2020-03-06 16:47:34 +0900

    Update pets_controller.rb
    changed index

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,8 +2,8 @@ class PetsController < ApplicationController
   before_action :set_pet, only: [:show, :edit, :update, :delete]
 
   def index
-    # @pets = Pet.where.not(user: current_user.excluded_friends, id: current_user.pet)
-    @pets = Pet.where.not(user: current_user.excluded_friends, id: current_user.pet).first(5)
+    @pets = Pet.where.not(user: current_user.excluded_friends, id: current_user.pet).order('name ASC')
+    # @pets = Pet.where.not(user: current_user.excluded_friends, id: current_user.pet).first(5)
     # @pets = Pet.all
   end
 
