b696e94456c91ee97a0b8cdad2298b3c2891ddb4
Junya Nomura <jm18l028@g.hit-u.ac.jp>
2020-03-10 15:04:42 +0900

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://woofer.fun/cable'
-  config.action_cable.allowed_request_origins = [ 'http://woofer.fun', /http:\/\/woofer.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://woofer.fun', /http:\/\/woofer.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
