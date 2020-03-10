86b27727187911e926e0738c864a3a505a58a2dd
Shoichi Hasegawa <shoichihasegawa2018@hotmail.com>
2020-03-10 13:51:05 +0900

    added seed checked condition changed body font color added meta data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,23 +14,27 @@ User.destroy_all
 # <<<<<<< HEAD
 # puts "creating users"
 # =======
+
+
+
 user_all_name = ["Person1","Person2","Person3","Person4","Person5","Person6","Person7","Person8","Person9","Person10",]
 pet_all_name = ["Pet1","Pet2","Pet3","Pet4","Pet5","Pet6","Pet7","Pet8","Pet9","Pet10",]
 
 puts "creating users"
 
 user_urls = {
-1 => "https://cdn5.vectorstock.com/i/1000x1000/21/29/colorful-caricature-thin-man-in-clothes-with-beard-vector-16102129.jpg",
-2 => "https://cdn5.vectorstock.com/i/1000x1000/82/04/colorful-caricature-side-view-young-man-with-vector-14938204.jpg",
-3 => "https://cdn1.vectorstock.com/i/1000x1000/79/75/color-image-caricature-full-body-male-person-with-vector-14767975.jpg",
-4 => "https://previews.123rf.com/images/jemastock/jemastock1612/jemastock161203274/67266116-inspector-man-cartoon-icon-comic-character-and-caricature-theme-isolated-design-vector-illustration.jpg",
-5 => "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRFDO_G-WLQFpkJ5Vp5M_Sn830uH6llQPKqZeljsJxGM9c35o11",
-6 => "https://previews.123rf.com/images/jemastock/jemastock1706/jemastock170610298/80262778-man-cartoon-face-adult-caricature-character-vector-illustration.jpg",
-7 => "https://cdn2.vectorstock.com/i/1000x1000/24/11/colorful-image-caricature-full-body-guy-sit-in-a-vector-14832411.jpg",
-8 => "https://c8.alamy.com/comp/MCJ9BN/young-asian-man-cartoon-MCJ9BN.jpg",
-9 => "https://c8.alamy.com/comp/MP93H5/happy-young-man-cartoon-scribble-MP93H5.jpg",
-10 => "https://www.wikihow.com/images/thumb/f/ff/Draw-a-Cute-Cartoon-Person-Step-14.jpg/aid459745-v4-1200px-Draw-a-Cute-Cartoon-Person-Step-14.jpg"
+1 => "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+2 => "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+3 => "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+4 => "https://images.pexels.com/photos/936229/pexels-photo-936229.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+5 => "https://images.pexels.com/photos/3775534/pexels-photo-3775534.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+6 => "https://images.pexels.com/photos/3874039/pexels-photo-3874039.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+7 => "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+8 => "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+9 => "https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+10 => "https://images.pexels.com/photos/1680317/pexels-photo-1680317.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
 }
+
 
 
 (1..10).each do |number|
@@ -38,7 +42,7 @@ user_urls = {
  p url = user_urls[number]
 
   user = User.create!(
-    first_name: ["Antonio", "Sho", "Junya", "TJ", "Yann", "Doug", "Sylvain", "Hiro", "Jethro", "Ant"].sample,
+    first_name: ["Simon", "TJ", "Yann", "Thomas", "Apple", "Matt", "Janice", "Ant", "Johnson", "Jim"].sample,
     last_name: Faker::Name.unique.last_name ,
     location: ["Meguro", "Ebisu", "Shinagawa", "Shinjuku", "Shibuya","Tokyo","Shinbashi","Ikebukuro","Daikanyama","Gotanda","Oosaki"].sample,
     email: Faker::Internet.email,
@@ -102,4 +106,54 @@ User.all.each_with_index do |user, index|
       end
   end
 end
+
+antonio = User.create!(
+    first_name: "Antonio",
+    last_name: "Sanchez",
+    location: "Musashikosugi",
+    email: "antonio.sanchez@lewagon.com",
+    password: "123456"
+    )
+    file = File.open(File.join(Rails.root,'app','assets','images','antonio.jpg'))
+    antonio.photo.attach(io: file, filename: "antonio.jpg", content_type: 'image/jpg')
+    antonio.save!
+    1.times do
+    pet = Pet.create!(
+      name: 'Pepa',
+      user: antonio,
+      gender: "female",
+      age: 10,
+      breed: Faker::Creature::Dog.breed,
+      description: "My dog doesn't like grass"
+      )
+    file = File.open(File.join(Rails.root,'app','assets','images','pepa.jpg'))
+    pet.photo.attach(io: file, filename: "pepa.jpg", content_type: 'image/jpg')
+    pet.save!
+    end
+
+junya= User.create!(
+    first_name: "Junya",
+    last_name: "Nomura" ,
+    location: "Azabu",
+    email: "junya.nomura@lewagon.com",
+    password: "123456"
+    )
+ file = File.open(File.join(Rails.root,'app','assets','images','junya.jpg'))
+    junya.photo.attach(io: file, filename: "junya.jpg", content_type: 'image/jpg')
+    antonio.save!
+    # junya.photo.attach())
+    junya.save!
+    1.times do
+    pet = Pet.create!(
+      name: 'Pepper',
+      user: junya,
+      gender: "male",
+      age: 15,
+      breed: Faker::Creature::Dog.breed,
+      description: "My dog doesn't like grass"
+      )
+    file = File.open(File.join(Rails.root,'app','assets','images','dog27.jpg'))
+    pet.photo.attach(io: file, filename: "dog27.jpg", content_type: 'image/jpg')
+    pet.save!
+    end
 puts "Finished"
